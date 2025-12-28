d991e480f1f5b642d06efad5152d8c88f4c266be
Brandon Knape <d.brandonk@gmail.com>
2025-12-28 10:26:36 -0600

    Removed fzf mention from docker.
    Also fixed fzf install in provision.sh.
    Rmoved fd for the time being.

--- provision.sh
+++ provision.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install $(cat bk-dev-docker/apt_packages.txt) -y --fix-missing
 
 # fixing install of fzf if there is an issue
-./.fzf/uninstall || true
-rm -rf ./.fzf
+~/.fzf/uninstall || true
+rm -rf ~/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all --no-update-rc
 
