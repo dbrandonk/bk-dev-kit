377829061fef2571b0798997d935ef2177281bca
Brandon Knape <d.brandonk@gmail.com>
2025-12-28 17:10:07 -0600

    Moved the apt_packages info to provision.sh.

--- provision.sh
+++ provision.sh
@@ -4,7 +4,32 @@
 set -eu
 
 sudo apt update
-sudo apt install $(cat bk-dev-docker/apt_packages.txt) -y --fix-missing
+sudo apt install -y --fix-missing \
+aspell \
+bash-completion \
+checkinstall \
+clang-tidy \
+universal-ctags \
+curl \
+feh \
+git \
+htop \
+man \
+python3-venv \
+stow \
+sudo \
+tig \
+tldr \
+tmux \
+tree \
+ranger \
+ripgrep \
+vim \
+vim-gtk3 \
+x11-xserver-utils \
+xclip \
+wget \
+yamllint
 
 # fixing install of fzf if there is an issue
 ~/.fzf/uninstall || true
