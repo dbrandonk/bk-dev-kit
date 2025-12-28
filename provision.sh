708615d67cef32eef1bf4124c0f2d182a97bc29b
Brandon Knape <d.brandonk@gmail.com>
2025-12-28 10:06:28 -0600

    Moved fzf to provision.sh.
    Also added fd and x11 packages. Removed ag.

--- provision.sh
+++ provision.sh
@@ -3,4 +3,10 @@
 sudo apt update
 sudo apt install $(cat bk-dev-docker/apt_packages.txt) -y --fix-missing
 
+# fixing install of fzf if there is an issue
+./.fzf/uninstall || true
+rm -rf ./.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
 # one day I should do gsettings stuff here.
